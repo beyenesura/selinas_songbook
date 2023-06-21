e9d0ba05b2aa90c522650ff82c56bbde49afd38d
Axl Cruz <axl.cruz8344@gmail.com>
2023-06-21 14:32:36 -0400

    added add song and get sone logic

--- front_end/react/components/App.js
+++ front_end/react/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { handleRegisterSubmit,handleLoginSubmit,handleAllSongs } from './api_calls';
+import { handleRegisterSubmit,handleLoginSubmit,handleAllSongs,handleOneSong,handleAddSong} from './api_calls';
 import { SongList } from './SongList';
 export const App = () => {
   // State holds the current file that is to be uploaded to the server
@@ -9,9 +9,16 @@ export const App = () => {
   const [someToken,setSomeToken] = useState()
   const [ifToken,setIfToken] = useState(false)
   const [allSongs,setAllSongs]= useState()
-  const [someSong,setSomeSong] = useState()
+  const [song,setSong] = useState()
+  const [returnedSong,setReturnedSong] = useState()
 
 
+
+  /* States for Adding Songs */
+
+  const [title,setTitle] =useState()
+  const [author,setAuthor] =useState()
+  const [lyrics,setLyrics] =useState()
 
 
 
@@ -92,8 +99,8 @@ export const App = () => {
       return (
         <div className="top_level_container">
           <button onClick={(e)=>handleAllSongs(e,setPage,setAllSongs,someToken)}>Get Songs</button>
-          <button onClick={(e)=>handleOneSong(e,setPage,someToken)}>Get A Song </button>
-          <button>Add A song</button>
+          <button onClick={(e)=>setPage('some_song')}>Get A Song </button>
+          <button onClick={(e)=>setPage('add_song')}>Add A song</button>
           <button>Edit A Song</button>
           <button>Delete A Song</button>
         </div>
@@ -118,9 +125,98 @@ export const App = () => {
 
     return (
         <div>
-          <SongList songlist={allSongs} setPG={setPage}/>
+         
+         <form onSubmit={(e) => handleOneSong(e,setPage,song,someToken,setReturnedSong)}>
+          <input type="text" onChange={(e) => setSong(e.target.value)} placeholder='song name'/>
+          <input type='submit' value='Find Song'></input>
+        </form>
+
+
         </div>
 
     )
+  }
+
+
+
+
+
+
+  else if(page==='song_returned' && !returnedSong){
+
+    return(
+    <div className="top_level_container">
+      <h3>Error on finding Song</h3>
+      <button onClick={(e)=>handleAllSongs(e,setPage,setAllSongs,someToken)}>Get Songs</button>
+      <button onClick={(e)=>setPage('some_song')}>Get A Song </button>
+      <button>Add A song</button>
+      <button>Edit A Song</button>
+      <button>Delete A Song</button>
+    </div>
+    )
+  
+
+  }else if(page==='song_returned'){
+    return(
+      <div>
+        <h3>{returnedSong}</h3>
+        <button onClick={(e)=>setPage('homepage')}>Homepage</button>
+      </div>
+    )
+  }
+
+  
+
+
+
+
+  else if(page==='add_song'){
+
+    return (
+        <div>
+         
+         <form onSubmit={(e) => handleAddSong(e,setPage,title,author,lyrics,someToken)}>
+          <input type="text" onChange={(e) => setTitle(e.target.value)} placeholder='song title'/>
+          <input type="text" onChange={(e) => setAuthor(e.target.value)} placeholder='author name'/>
+          <input type="text" onChange={(e) => setLyrics(e.target.value)} placeholder='lyrics'/>
+          <input type='submit' value='Add Song'></input>
+        </form>
+
+
+        </div>
+
+    )
+  }
+
+
+
+  else if(page==='added_song_error'){
+
+    return(
+      <div className="top_level_container">
+        <h3>Error on Adding Song</h3>
+        <button onClick={(e)=>handleAllSongs(e,setPage,setAllSongs,someToken)}>Get Songs</button>
+        <button onClick={(e)=>setPage('some_song')}>Get A Song </button>
+        <button onClick={(e)=>setPage('add_song')}>Add A song</button>
+        <button>Edit A Song</button>
+        <button>Delete A Song</button>
+      </div>
+      )
+
+  }
+
+  else if(page==='added_song_success'){
+
+    return(
+      <div className="top_level_container">
+        <h3>Song Added Successfully</h3>
+        <button onClick={(e)=>handleAllSongs(e,setPage,setAllSongs,someToken)}>Get Songs</button>
+        <button onClick={(e)=>setPage('some_song')}>Get A Song </button>
+        <button onClick={(e)=>setPage('add_song')}>Add A song</button>
+        <button>Edit A Song</button>
+        <button>Delete A Song</button>
+      </div>
+      )
+
   }
 }
--- front_end/react/components/api_calls.js
+++ front_end/react/components/api_calls.js
@@ -80,4 +80,69 @@ const handleAllSongs = (e,setPG,setAS,token) =>{
 }
 
 
-module.exports = {handleRegisterSubmit,handleLoginSubmit,handleAllSongs}
+
+
+const handleOneSong = (e,setPG,song,token,setRS) =>{
+    e.preventDefault()
+    full_search = 'http://127.0.0.1:5000/get_song?' + `song=${song}`
+    fetch(full_search,{
+        method:'GET',
+        headers: {
+            'x-access-token':token
+        },
+    })
+
+    .then((res)=>res.json())
+    .then((data)=>
+    {
+            console.log(data)
+            setRS(data['response'])
+            setPG('song_returned')
+    
+    })
+    .catch((err)=>console.error(err))
+    
+}
+
+
+
+
+
+
+
+const handleAddSong = (e,setPG,title,author,lyrics,token) =>{
+    e.preventDefault()
+    console.log(JSON.stringify({title:title,author:author,lyrics:lyrics}))
+    fetch('http://127.0.0.1:5000/add_song',{
+        method:'POST',
+        headers: {
+            'Content-type':'application/json', 
+            'Accept':'application/json',
+            'x-access-token':token
+        },
+        body:JSON.stringify({title:title,author:author,lyrics:lyrics})
+    })
+
+    .then((res)=>res.json())
+    .then((data)=>
+    {
+
+        console.log(data['success'])
+        if( !(data['success'])){
+            setPG('added_song_error')
+        }else{
+            setPG('added_song_success')
+        }
+
+
+
+            console.log(data)
+    
+    })
+    .catch((err)=>console.error(err))
+    
+}
+
+
+
+module.exports = {handleRegisterSubmit,handleLoginSubmit,handleAllSongs,handleOneSong,handleAddSong}
